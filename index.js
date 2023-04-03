0ab73c38660276eabd12d38998d973f1ad803aa3
Shivam Gupta <117441360+boy-scripter@users.noreply.github.com>
2023-04-03 07:12:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const express = require("express")
-const app = express()
-app.get("/", (req,res) => {
-    res.send("this is only for testing")
+const express = require('express')
+const RegisterRouter = require('./routers/RegisterRouter')
+const LoginRouter = require('./routers/LoginRouter')
+const cors = require("cors")
+const { ALLOWEDORIGIN } = require('./config')
+const app = express();
+const PORT = 3000 ;
+app.use(express.json());
+app.use(cors({
+    origin: ALLOWEDORIGIN
+}))
+
+app.all('/', (req, res) => {
+    res.status(200).send("this server is created by shivaNm")
 })
-app.listen(3000)
+
+
+app.use('/api', RegisterRouter);
+app.use('/api', LoginRouter);
+
+app.listen(PORT )
+
